28a0092f7221ff4820aef93f2711388034c256c8
Adhiraj Saha <adhirajfirst@gmail.com>
2023-08-20 20:22:51 +0530

    User authorisation and authentication.

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+
+# database
+include("inc/db.php");
+
+if (isset($_POST['login'])) {
+    $email = $_POST['loginEmail'];
+    $password = $_POST['loginPassword'];
+
+    #$hashedPassword = hash('sha256', $password);
+
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $rs = $conn->query($sql);
+
+    if ($rs->num_rows == 1) {
+        // Login successful
+        session_start();
+        $_SESSION['loggedin'] = true;
+        
+        header("Location: index.php");
+        exit();
+    } else {
+        // Login failed
+        $loginError = "Invalid email or password";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,8 +34,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HappyHolidayHome - Login</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.jpg">
     <link rel="stylesheet" href="assets/css/signup-login.css">
-    <script src="assets/js/login.js"></script>
+    
 </head>
 
 <body>
@@ -15,15 +45,21 @@
     <?php include("inc/header.php"); ?>
 
     <div class="container">
-        <form action="/" method="POST" class="form" onsubmit="return validateLoginForm()">
+        <form action="" method="POST" class="form">
             <label>Enter your Email ID:</label>
             <input type="email" id="loginEmail" name="loginEmail" placeholder="username@abc.com" required>
             <label>Enter your Password:</label>
             <input type="password" id="loginPassword" name="loginPassword" required>
+            <?php
+            if (isset($loginError)) {
+                echo "<p class='error' style='color: red; font-weight: bold;'>$loginError</p>";
+            }
+            ?>
             <button type="submit" name="login" class="btn btn-success">Login</button>
             <button type="reset" name="reset" class="btn btn-danger">Cancel</button>
             <p class="no-account">Don't have an account? <a href="signup.php">Sign up here</a></p>
         </form>
+
 
     </div>
     
--- signup.php
+++ signup.php
@@ -1,22 +1,57 @@
+<?php
+
+# database
+include("inc/db.php");
+
+
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirmPassword'];
+
+    // Perform password validation and hashing
+    if ($password === $confirmPassword) {
+        #password = hash('sha256', $password);
+        
+        $insertQuery = "INSERT INTO users (name, email, phone, password) 
+                        VALUES ('$name', '$email', '$phone', '$password')";
+        
+        if (mysqli_query($conn, $insertQuery)) {
+            // Registration successful
+            $_SESSION['registrationSuccess'] = true;
+            header("Location: index.php");
+            exit();
+        } else {
+            $registrationError = "Registration failed. Please try again.";
+        }
+    } else {
+        $registrationError = "Passwords do not match.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HappyHolidayHome</title>
+    <title>HappyHolidayHome - Sign Up</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.jpg">
     <link rel="stylesheet" href="assets/css/signup-login.css">
-    <script src="assets/js/signup.js"></script>
+    
 </head>
 
 <body">
      
      <!-- Nav Bar -->
     <?php include("inc/header.php"); ?>
-    <br><br>
+    <br><br><br>
 
     <div class="container">
-        <form action="/" method="POST" class="form" onsubmit="return validateForm()">
+        <form action="" method="POST" class="form">
             <label>Enter your Name:</label>
             <input type="text" id="name" name="name" placeholder="Your Full Name" required>
             <label>Enter your Email ID:</label>
@@ -27,6 +62,11 @@
             <input type="password" id="password" name="password" required>
             <label>Confirm Password:</label>
             <input type="password" id="confirmPassword" name="confirmPassword" required>
+            <?php
+            if (isset($registrationError)) {
+                echo "<p class='error' style='color: red; font-weight: bold;'>$registrationError</p>";
+            }
+            ?>
             <button type="submit" name="save" class="btn btn-success">Sign Up</button>
             <button type="reset" name="reset" class="btn btn-danger">Cancel</button>
             <p class="no-account">Already have Account? <a href="login.php">Login here</a></p>
