99c304a6576640ddf9f2bcd0fe5723df06707405
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2023-08-28 15:44:45 +0000

    updated the database schema

--- database_schema/hhh_db.sql
+++ database_schema/hhh_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2023 at 05:13 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Aug 28, 2023 at 12:59 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,18 +37,18 @@ CREATE TABLE `holiday_homes` (
   `rating` decimal(10,1) NOT NULL,
   `image_path` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `holiday_homes`
 --
 
 INSERT INTO `holiday_homes` (`home_id`, `name`, `location`, `availability_start`, `availability_end`, `description`, `rating`, `image_path`, `price`) VALUES
-(1, 'Luxury villa', 'Florida', '2023-08-02', '2023-08-31', 'This luxury holiday home in Florida, USA is filled to the brim with an array of amazing facilities and luxury amenities. With 15,000 square feet of space, the unique mansion is just over five miles from Disneyland and has views over the world-famous Jack Nicklaus golf course. But there\'s something surprising here that may keep you indoors.', 4.0, 'assets/img/holiday-homes/Luxury-villa.jpg', 500.00),
-(2, 'Downtown garden apartment', 'Florida', '2023-08-01', '2023-09-07', 'This modern apartment in Buena Vista, Colorado is the ideal space for a weekend getaway and holds a secret inside that\'s perfect for the active holidaymaker. With its own garden and seating area, it can sleep up to four people and has all the latest modern amenities.', 4.2, 'assets/img/holiday-homes/Downtown-garden-apartment.jpg', 400.00),
-(3, 'Luxury villa2', 'Florida', '2023-08-01', '2023-08-31', 'However, hidden deep in the house is the ultimate in luxury entertainment: a bowling alley! With two lanes, it\'s the perfect place to get competitive and even has a comfortable lounge space and TV screens so you can keep track of who\'s winning. At £2,052 ($2,608) a night, with a minimum stay of four nights, it\'ll be an expensive game of bowling. ', 4.3, 'assets/img/holiday-homes/Luxury-villa2.jpg', 300.00),
-(4, 'City escape', 'Florida', '2023-08-01', '2023-08-31', 'Designed as a break from busy city life, guests walk into a magical forest where the relaxing living room features a log stool and a fun swinging chair. The enchanting woodland doesn\'t come without modern-day amenities and includes a smart TV and air-conditioning. ', 3.9, 'assets/img/holiday-homes/City-escape.jpg', 360.00),
-(5, 'Domus Civita', 'Florida', '2023-08-01', '2023-08-29', 'Located on the first floor of the house, the living room and kitchen are full of original features including a basalt stone fireplace, terracotta floors and wooden beams, which all date from the 14th century. Overall the villa can sleep six people in three bedrooms which are surrounded by arched doorways and exposed tufa rock.', 3.7, 'assets/img/holiday-homes/Domus-Civita.jpg', 470.00);
+(1, 'Luxury villa', 'Florida', '2023-08-02', '2023-08-31', 'This luxury holiday home in Florida, USA is filled to the brim with an array of amazing facilities and luxury amenities. With 15,000 square feet of space, the unique mansion is just over five miles from Disneyland and has views over the world-famous Jack Nicklaus golf course. But there\'s something surprising here that may keep you indoors.', '4.0', 'assets/img/holiday-homes/Luxury-villa.jpg', '500.00'),
+(2, 'Downtown garden apartment', 'Florida', '2023-08-01', '2023-09-07', 'This modern apartment in Buena Vista, Colorado is the ideal space for a weekend getaway and holds a secret inside that\'s perfect for the active holidaymaker. With its own garden and seating area, it can sleep up to four people and has all the latest modern amenities.', '4.2', 'assets/img/holiday-homes/Downtown-garden-apartment.jpg', '400.00'),
+(3, 'Luxury villa2', 'Florida', '2023-08-01', '2023-08-31', 'However, hidden deep in the house is the ultimate in luxury entertainment: a bowling alley! With two lanes, it\'s the perfect place to get competitive and even has a comfortable lounge space and TV screens so you can keep track of who\'s winning. At £2,052 ($2,608) a night, with a minimum stay of four nights, it\'ll be an expensive game of bowling. ', '4.3', 'assets/img/holiday-homes/Luxury-villa2.jpg', '300.00'),
+(4, 'City escape', 'Florida', '2023-08-01', '2023-08-31', 'Designed as a break from busy city life, guests walk into a magical forest where the relaxing living room features a log stool and a fun swinging chair. The enchanting woodland doesn\'t come without modern-day amenities and includes a smart TV and air-conditioning. ', '3.9', 'assets/img/holiday-homes/City-escape.jpg', '360.00'),
+(5, 'Domus Civita', 'Florida', '2023-08-01', '2023-08-29', 'Located on the first floor of the house, the living room and kitchen are full of original features including a basalt stone fireplace, terracotta floors and wooden beams, which all date from the 14th century. Overall the villa can sleep six people in three bedrooms which are surrounded by arched doorways and exposed tufa rock.', '3.7', 'assets/img/holiday-homes/Domus-Civita.jpg', '470.00');
 
 -- --------------------------------------------------------
 
@@ -63,15 +63,15 @@ CREATE TABLE `reservations` (
   `check_in_date` date NOT NULL,
   `check_out_date` date NOT NULL,
   `total_price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `reservations`
 --
 
 INSERT INTO `reservations` (`reservation_id`, `user_id`, `home_id`, `check_in_date`, `check_out_date`, `total_price`) VALUES
-(1, 1, 2, '2023-08-16', '2023-08-23', 2800.00),
-(2, 1, 1, '2023-08-16', '2023-08-23', 3500.00);
+(3, 1, 3, '2023-08-15', '2023-08-23', '2400.00'),
+(4, 1, 2, '2023-08-16', '2023-08-30', '5600.00');
 
 -- --------------------------------------------------------
 
@@ -85,14 +85,14 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `phone` int(20) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `phone`, `password`) VALUES
-(1, 'adhi', 'a@gmail.com', 1234567890, '1234'),
+(1, 'Adhi', 'a@gmail.com', 1234567890, '1234'),
 (2, 'WoofBot', 'adhirajfirst@gmail.com', 2147483647, '1234'),
 (3, 'rhito', 'rhito@gmail.com', 2147483647, '1234');
 
@@ -134,7 +134,7 @@ ALTER TABLE `holiday_homes`
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
